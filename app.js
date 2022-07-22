72a534ef85e1bcbfd6dc05aee8a0ed181ae5356b
Yiğit <ytan2877@gmail.com>
2022-07-22 21:06:37 +0300

    something is wrong about connecting mongodb

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const path = require("path");
-const ejs = require("ejs"); 
+const ejs = require("ejs");
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose.connect(`mongodb://localhost:${port}/pcat-db-test`, {
+  useNewUrlParser:true,
+  useunifiedTopology:true,
+});
 
 const port = 3000;
 // TEMPLATE ENGİNE
@@ -10,6 +17,9 @@ app.set("view engine","ejs");
 
 // MIDDLEWARES
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 
 // ROUTES
 app.get("/", (req,res) => {
@@ -24,6 +34,12 @@ app.get("/add", (req,res) => {
   res.render("add") //(index = index.ejs)
 })
 
+app.post("/photos", (req,res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
 app.listen(port, () => {
   console.log(`server created at ${port}`);
 });
+
